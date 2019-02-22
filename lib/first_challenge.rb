bb80b761c973c13f86680f97312135f90a809c6e
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-22 16:49:50 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -39,7 +39,9 @@ def first_challenge
         :age => "51"
       },
       :heroine => {
-        
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
       },
       :heroine_friends => []
    }
