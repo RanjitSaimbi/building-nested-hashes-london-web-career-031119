98bef469aa2a77289eb65e12d804df8d40b77d16
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-22 16:46:52 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -4,12 +4,29 @@ def first_challenge
       :patriarch => {
         {
           :name => "Lord Montague"
-          :age => "33"
+          :age => "53"
         }
       },
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
+      :matriarch => {
+        :name => "Lady Montague"
+        :age => "54"
+      },
+      :hero => {
+        :name => "Romeo"
+        :age => "15"
+      },
+      :hero_friends => [
+      {
+        :name => "Benvolio"
+        :age => "17"
+        :attitude => "worried"
+      },
+      {
+        :name => "Mercutio"
+        :age => "18"
+        :attitude => "hot-headed"
+        }
+      ]
    },
    :capulet => {
       :patriarch => {},
